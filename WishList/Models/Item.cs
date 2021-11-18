25d7c7067f8665e81fbdf63af6a39aa594445bfc
Warren <wvandentop@drummondco.com>
2021-11-18 09:16:41 -0600

    6

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -6,8 +6,11 @@ namespace WishList.Models
     {
         public int Id { get; set; }
 
+        public virtual ApplicationUser User { get; set; }
+
         [Required]
         [MaxLength(50)]
         public string Description { get; set; }
+
     }
 }
