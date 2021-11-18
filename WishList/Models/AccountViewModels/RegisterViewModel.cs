43df709a71e2b062e9940517ed60a7c67a82454f
Warren <wvandentop@drummondco.com>
2021-11-18 08:49:11 -0600

    3

--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -14,7 +14,7 @@ namespace WishList.Models.AccountViewModels
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100)]
+        [StringLength(100, MinimumLength = 8)]
         [MinLength(8)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
